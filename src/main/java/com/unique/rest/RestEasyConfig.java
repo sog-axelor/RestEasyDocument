aeeb2ad2001ba36202b56ce34300c86113a2eb02
Sonu <s.gupta@axelor.com>
2023-01-25 13:59:53 +0530

    intial commit

---
+++ src/main/java/com/unique/rest/RestEasyConfig.java
@@ -0,0 +1,21 @@
+package com.unique.rest;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/restApi")
+public class RestEasyConfig extends Application {
+	
+   private Set<Object> singletons  = new HashSet<>();
+   
+   public RestEasyConfig() {
+	   this.singletons.add(new HelloResource());
+   }
+   @Override
+   public Set<Object> getSingletons(){
+	   return singletons;
+   }
+}
